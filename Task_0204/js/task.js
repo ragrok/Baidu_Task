d61e3b113926c7727d760287514b984adf32c850
ragrok <ragrok.lee@hotmail.com>
2016-08-23 20:52:56 +0800

    init task_0402

--- Task_0204/js/task.js
+++ Task_0204/js/task.js
@@ -11,20 +11,41 @@ var aqiData = {};
 var $ = function (id) {
     return document.getElementById(id);
 }
+
+var city_input = $('aqi-city-input');
+var value_input = $('aqi-value-input');
+var aqi_table = $('aqi-table');
+
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
  */
 function addAqiData() {
-   let city_input = $('aqi-city-input').value;
-    let value_input = $('aqi-value-input').value;
+
+    let city_value = city_input.value.trim();
+    let air_value = value_input.value.trim();
+     //验证中文
+    if (!/[a-zA-Z\u4e00-\u9fa5]/.test(city_value)) {
+        alert('请输入字母或者中文');
+        return;
+    }
+    //验证数字
+    if (!/\d{1,}/.test(air_value)) {
+        alert('请输入数字');
+        return;
+    }
+    aqiData[city_value] = air_value;
+    console.log(aqiData);
 }
 
 /**
  * 渲染aqi-table表格
  */
 function renderAqiList() {
-
+    console.log(aqiData[city_input.value.trim()]);
+    console.log(aqiData.keys());
+    console.log(aqiData.values());
+    console.log(aqiData.entries());
 }
 
 /**
@@ -49,9 +70,11 @@ function delBtnHandle() {
 function init() {
 
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
-
+    $('add-btn').onclick = function () {
+        addAqiData();
+        renderAqiList();
+    }
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
-
 }
 
 init();
